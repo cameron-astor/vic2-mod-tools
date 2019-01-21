ebef47145762b7f20bd8405defbca3f45330342e
Cameron Astor <cameron.astor@gmail.com>
2019-01-21 00:16:04 -0800

    add Peasantizer class skeleton

---
+++ src/vic2_tools/Peasantizer.java
@@ -0,0 +1,11 @@
+package vic2_tools;
+//A population editing concept.
+//For every province: total farmers, turn 30% of them into peasants.
+
+public class Peasantizer {
+
+	public static void main(String[] args) {
+
+	}
+
+}
